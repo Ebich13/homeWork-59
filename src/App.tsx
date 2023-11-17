cfc49a42df7c3f473fb7b2247119c0f9a7724a3a
Kubat <kubas9214@gmail.com>
2023-11-17 08:12:17 +0600

    Edit Joke.app render!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import React, {useState} from 'react';
+import JokeButton from './components/Joke/JokeButton';
+import JokeDisplay from './components/Joke/JokeDisplay';
+import {Jokes} from "./types";
+
+const url = 'https://api.chucknorris.io/jokes/random';
+
 function App() {
+  const [jokes, setJokes] = useState<Jokes[]>([]);
+  const [loading, setLoading] = useState(false);
 
+  const fetchJokes = async () => {
+    try {
+      setLoading(true);
+      const responses = await Promise.all(
+        Array.from({ length: 5 }, () => fetch(url).then(response => response.json()))
+      );
 
+      const newJokes = responses.map(data => data.value);
+      setJokes(newJokes);
+    } catch (error) {
+      console.error('Error fetching jokes:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <div>
-      test
+    <div className="col-md-6">
+      <JokeButton onClick={fetchJokes} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        jokes.map((joke, index) => (
+          <JokeDisplay key={index} joke={joke} />
+        ))
+      )}
     </div>
+
 
   );
 }
